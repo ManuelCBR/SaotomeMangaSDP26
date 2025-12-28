69b24ad4c18beed68d3c5c7c21b0689095900bc6
ManuelCBR <manuelvs1987@gmail.com>
2025-12-28 10:49:40 +0100

    Initial Commit

---
+++ SaotomeMangaSDP26/SaotomeMangaSDP26App.swift
@@ -0,0 +1,17 @@
+//
+//  SaotomeMangaSDP26App.swift
+//  SaotomeMangaSDP26
+//
+//  Created by Manuel Bermudo on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SaotomeMangaSDP26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
